9ada014a537b0cd605d079d9cbf2be883003a746
Federico Calvette <calvettefederico@gmail.com>
2024-07-09 22:18:22 -0300

    add new model

---
+++ new.py
@@ -0,0 +1,20 @@
+class New:
+    def __init__(self, header, title, img, web, text, category, date):
+        self.header = header
+        self.title = title
+        self.img = img
+        self.web = web
+        self.text = text
+        self.category = category
+        self.date = date
+        
+    def to_json(self):
+        return {
+            "header": self.header,
+            "titulo": self.title,
+            "img": self.img,
+            "web": self.web,
+            "texto": self.text,
+            "categorias": self.category,
+            "fecha": self.date
+        }
