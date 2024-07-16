3c6768ba0b43a94ebabd3d5d6b4eed514a1a9512
BruMaster7 <brunorocha1ero4@gmail.com>
2024-07-16 17:31:42 -0300

    added el_pais to the refactoring

--- src/noti_scraperapi/scrapers.py
+++ src/noti_scraperapi/scrapers.py
@@ -1,3 +1,4 @@
+from noti_scraperapi.article_parsers.el_pais import ElPaisParser
 from noti_scraperapi.article_parsers.montevideo_portal import MontevideoPortalParser
 from noti_scraperapi.article_parsers.xataka import XatakaArticleParser
 from noti_scraperapi.util import ScraperSettings, run_scraper
@@ -19,6 +20,14 @@ SCRAPERS = {
         filename="montevideo_portal",
         web_name="Montevideo Portal",
     ),
+    "El Pais": ScraperSettings(
+        url="https://www.elpais.com.uy/",
+        html_tag="div",
+        class_name="PromoBasic-content",
+        article_parser=ElPaisParser,
+        filename="el_pais",
+        web_name="El Pais",
+    ),
 }
 
 
@@ -28,3 +37,7 @@ def run_xataka_scraper():
 
 def run_montevideo_portal_scraper():
     run_scraper(SCRAPERS["Montevideo Portal"])
+
+
+def run_el_pais_scraper():
+    run_scraper(SCRAPERS["El Pais"])
