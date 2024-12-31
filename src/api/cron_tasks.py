b0a2f0a471ebfae4133d78e44af0c0a1a59bb7c5
BruMaster7 <brunorocha1ero4@gmail.com>
2024-12-31 04:13:29 -0300

    Add Cron Tasks

---
+++ src/api/cron_tasks.py
@@ -0,0 +1,32 @@
+import os
+import schedule
+import time
+
+def run_el_pais():
+    os.system("rye run el_pais")
+    print("Ejecutado el scraper: El Pais")
+
+def run_montevideo_portal():
+    os.system("rye run montevideo_portal")
+    print("Ejecutado el scraper: Montevideo Portal")
+
+def run_xataka():
+    os.system("rye run xataka")
+    print("Ejecutado el scraper: Xataka")
+
+def connect_to_mongo():
+    os.system("rye run connect_to_mongo")
+    print("Conectado a MongoDB y datos actualizados")
+
+def schedule_tasks():
+    schedule.every(6).hours.do(run_el_pais)  # Ejecuta cada 6 horas
+    schedule.every(6).hours.do(run_montevideo_portal)
+    schedule.every(6).hours.do(run_xataka)
+    schedule.every(6).hours.do(connect_to_mongo)
+
+    print("Tasks scheduled to run every 6 hours.")
+
+def run_scheduler():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
