a85e6cff3d059a74d57bed9e5b146a6f9b66593e
BruMaster7 <brunorocha1ero4@gmail.com>
2025-01-03 02:56:17 -0300

    Fix scrapers run for Docker

--- dockerfile
+++ dockerfile
@@ -1,10 +1,8 @@
 FROM python:3.12-slim
 
-# Instalar Rye
-RUN curl -sSf https://install.rye-up.com | bash
-ENV PATH="/root/.rye/bin:$PATH"
-
 WORKDIR /app
+
+ENV PYTHONPATH=/app/src
 
 COPY requirements.lock .
 RUN pip install --no-cache-dir -r requirements.lock
--- src/api/cron_tasks.py
+++ src/api/cron_tasks.py
@@ -3,15 +3,15 @@ import schedule
 import time
 
 def run_el_pais():
-    os.system("python -m noti_scraperapi.scrapers run_el_pais_scraper")
+    os.system('python -c "from noti_scraperapi.scrapers import run_el_pais_scraper; run_el_pais_scraper()"')
     print("Ejecutado el scraper: El Pais")
 
 def run_montevideo_portal():
-    os.system("python -m noti_scraperapi.scrapers run_montevideo_portal_scraper")
+    os.system('python -c "from noti_scraperapi.scrapers import run_montevideo_portal_scraper; run_montevideo_portal_scraper()"')
     print("Ejecutado el scraper: Montevideo Portal")
 
 def run_xataka():
-    os.system("python -m noti_scraperapi.scrapers run_xataka_scraper")
+    os.system('python -c "from noti_scraperapi.scrapers import run_xataka_scraper; run_xataka_scraper()"')
     print("Ejecutado el scraper: Xataka")
 
 def connect_to_mongo():
@@ -19,10 +19,10 @@ def connect_to_mongo():
     print("Conectado a MongoDB y datos actualizados")
 
 def schedule_tasks():
-    schedule.every(6).hours.do(run_el_pais)  # Ejecuta cada 6 horas
-    schedule.every(6).hours.do(run_montevideo_portal)
-    schedule.every(6).hours.do(run_xataka)
-    schedule.every(6).hours.do(connect_to_mongo)
+    schedule.every(1).hours.do(run_el_pais)  # Ejecuta cada 6 horas
+    schedule.every(1).hours.do(run_montevideo_portal)
+    schedule.every(1).hours.do(run_xataka)
+    schedule.every(1).hours.do(connect_to_mongo)
 
     print("Tasks scheduled to run every 6 hours.")
 
